696ecf030a7d8a26a3e812cfd24bd78145cedc50
Armando <41197882+mando8@users.noreply.github.com>
2023-07-26 02:01:59 -0600

    forgot to update comments

--- code/lookuptable.h
+++ code/lookuptable.h
@@ -6,17 +6,6 @@
 
 using namespace std;
 
-//
-//  This defines the names of various sorting functions and related
-//  helper functions. You are required to implement all of these.
-//
-//  When the user calls the sorting functions, it is expected that the
-//  output data is sorted in _nondecreasing_ order.  E.g.
-//
-//  input:  9, 5, 4, 1, 10, 5
-//  output: 1, 4, 5, 5, 9, 10 
-//
-
 class LookupTable {
 public:
   // constructor, initialize class variables and pointers here if need.
@@ -26,15 +15,16 @@ public:
   // freeing memory and you can leave this blank
   ~LookupTable();
 
-  // quicksort is the entry point for the quicksort routine. It should
-  // use the quicksort_partition function. It is also recursive, so you
-  // will call quicksort from inside the quicksort function.
+  // used to convert string to any binary, decimal, or hex
   string to_not_string(string strings, string type);
 
+  // used to convert binary to string
   string to_string_bin(string binary_code);
 
+  // used to convert decimal to string
   string to_string_dec(string decimal_code);
 
+  // used to convert hex to string
   string to_string_hex(string hex_code);
 
 private:
